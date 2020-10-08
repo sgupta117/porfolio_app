0d475b694f86b68c6ee91daf933f77174fb2c60b
Shubham Gupta <61824566+sgupta117@users.noreply.github.com>
2020-10-09 00:15:18 +0530

    Add files via upload

--- portfolio/views.py
+++ portfolio/views.py
@@ -16,28 +16,28 @@ def index(request):
     mrs = Projects()
     mrs.img = 'IPL.jpg'
     mrs.name = 'IPL MATCH SCORE PREDICTOR'
-    mrs.desc = 'Machine Learning Project that predicts the IPL match score after 20 overs'
+    mrs.desc = 'Machine Learning Project that predicts the IPL match score after 20 overs.'
     mrs.liveDemo = 'https://ipl-score-predictor-app.herokuapp.com/'
     mrs.codeLink = 'https://github.com/sgupta117/IPL-Score-Predictor'
 
     sf = Projects()
     sf.img = 'twitter.png'
     sf.name = 'TWITTER SENTIMENT ANALYSIS'
-    sf.desc = "A Cool web app that fetches the last 10 recent tweets of your favourite personality over tweeter with sentiment analysis of the tweets"
+    sf.desc = "A Cool web app that fetches the last 10 recent tweets of your favourite personality over tweeter with sentiment analysis of the tweets."
     sf.liveDemo = 'https://tweeter-analyzer-app.herokuapp.com/'
     sf.codeLink = 'https://github.com/sgupta117/Tweet-Analyzer'
 
     hms = Projects()
     hms.img = 'RPS.jpg'
     hms.name = 'ROCK PAPER SCISSOR GAME'
-    hms.desc = "Building a Rock Paper Scissors AI using Tensorflow and OpenCV with Python"
+    hms.desc = "Building a Rock Paper Scissors AI using Tensorflow and OpenCV with Python."
     hms.liveDemo = ''
     hms.codeLink = 'https://github.com/sgupta117/Rock-Paper-Scissor-ML-Game'
 
     dl = Projects()
     dl.img = 'Stock.png'
     dl.name = 'STOCK PRICE PREDICTION'
-    dl.desc = "Bilding a system to predict future stock prices for APPLE company using LSTM Recurrent Neural Network"
+    dl.desc = "Bilding a system to predict future stock prices for APPLE company using LSTM Recurrent Neural Network."
     dl.liveDemo = ''
     dl.codeLink = 'https://github.com/sgupta117/Deep-Learning-Projects/tree/master/LSTM/Stock-MArket-Forecasting-master_lstm'
 
@@ -51,28 +51,28 @@ def index(request):
     ga = Projects()
     ga.img = 'object.png'
     ga.name = 'OBJECT DETECTION USING YOLO'
-    ga.desc = "A python project to detect the real time objects on live video streaming using populat YOLO framework"
+    ga.desc = "A python project to detect the real time objects on live video streaming using popular YOLO framework."
     ga.liveDemo = ''
     ga.codeLink = 'https://github.com/sgupta117/OPEN-CV-Computer-Vision-Projects/tree/master/Object-Detection-Using-YOLO-3-master'
 
     bcp = Projects()
     bcp.img = 'mask.jpg'
     bcp.name = 'FACE MASK DETECTOR'
-    bcp.desc = "Building an accurate COVID-19 face mask detector with OpenCV, Keras/TensorFlow, and Deep Learning"
+    bcp.desc = "Building an accurate COVID-19 face mask detector with OpenCV, Keras/TensorFlow, and Deep Learning."
     bcp.liveDemo = ''
     bcp.codeLink = 'https://github.com/sgupta117/OPEN-CV-Computer-Vision-Projects/tree/master/Face-Mask-Detector-master'
 
     ad = Projects()
     ad.img = 'loan.jpg'
     ad.name = "LOAN ELIGIBILITY APP"
-    ad.desc = "A web app to check if a person is eligible for taking loan or not based upon the credit score,income and family background"
+    ad.desc = "A web app to check if a person is eligible for taking loan or not based upon the credit score,income and family background."
     ad.liveDemo = 'https://loan-eligibility-check.herokuapp.com/'
     ad.codeLink = 'https://github.com/sgupta117/Machine-Learning-Projects/tree/master/Loan-ELigibility-Checker-Web-App-master'
 
     ad1 = Projects()
     ad1.img = 'news.jpg'
     ad1.name = "FAKE NEWS CLASSIFIER"
-    ad1.desc = "Building a web app to predict the Fake News using Natural Language Processing"
+    ad1.desc = "Building a web app to predict the Fake News using Natural Language Processing."
     ad1.liveDemo = 'https://fake-news-classifier-nlp.herokuapp.com/'
     ad1.codeLink = 'https://github.com/sgupta117/Machine-Learning-Projects/tree/master/Fake-News-Classfier-master'
 
@@ -86,13 +86,13 @@ def index(request):
     rel = Blogs()
     rel.img = "random_forest.jpeg"
     rel.name = "Random Forest (Easily Explained)"
-    rel.desc = "Simple implementation and understanding of Random Forest algorithm with alongside python code."
+    rel.desc = "Simple implementation and understanding of Random Forest algorithm alongside python code."
     rel.link = "https://medium.com/@gupta020295/random-forest-easily-explained-4b8094feb90"
 
     dtr = Blogs()
     dtr.img = "imbalanced.jpeg"
     dtr.name = "Handle Imbalanced Dataset"
-    dtr.desc = "Blog to make people understand the importance of handling imbalanced dataset while preprocessing the data for any given ML problem statement"
+    dtr.desc = "Blog to make people understand the importance of handling imbalanced dataset while preprocessing the data for any given ML problem statement."
     dtr.link = "https://medium.com/@gupta020295/handle-imbalanced-dataset-5d4b072b8b36"
 
     gl = Blogs()
